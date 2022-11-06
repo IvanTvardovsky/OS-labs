0bde6f25012e4e3283f630403f33da13525f75c1
IvanTvardovsky <ivan.tvardovsky@gmail.com>
2022-11-06 15:12:01 +0300

    Time tests and fixes

--- tests/lab3_test.cpp
+++ tests/lab3_test.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <time.h>
 
 #include <array>
 #include <filesystem>
@@ -27,11 +28,36 @@ TEST(ThirdLabTests, InCircleTest) {
     EXPECT_EQ(in_circle(2, 3, 5), 1);
 }
 
-
 TEST(ThirdLabTests, GeneralTest) {
     EXPECT_EQ(isBetween(78, 79, function(5, 5)), 1);
     EXPECT_EQ(isBetween(78, 79, function(5, 1)), 1);
     EXPECT_EQ(isBetween(78, 79, function(5, 9)), 1);
     EXPECT_EQ(isBetween(0, 0, function(0, 5)), 1);
     EXPECT_EQ(isBetween(80, 81, function(5, 5)), 0);
+}
+
+TEST(ThirdLabTests, GeneralTimeTest) {
+    time_t t01 = time(0);
+    function(10000, 1);
+    time_t t02 = time(0);
+    double time_in_seconds1 = difftime(t01, t02);
+
+    time_t t11 = time(0);
+    function(10000, 5000);
+    time_t t12 = time(0);
+    double time_in_seconds2 = difftime(t11, t12);
+
+    EXPECT_EQ(time_in_seconds1 < time_in_seconds2, 1);
+
+    t01 = time(0);
+    function(5000, 1);
+    t02 = time(0);
+    time_in_seconds1 = difftime(t01, t02);
+
+    t11 = time(0);
+    function(5000, 5000);
+    t12 = time(0);
+    time_in_seconds2 = difftime(t11, t12);
+
+    EXPECT_EQ(time_in_seconds1 < time_in_seconds2, 1);
 }
